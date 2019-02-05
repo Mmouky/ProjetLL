c5d881edaa9266ca324022e8296cf6c43bdd343e
Mmouky <33157839+Mmouky@users.noreply.github.com>
2019-02-05 12:58:22 +0100

    route welcome

---
+++ limit/resources/views/template.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    @yield('title')
+
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+
+</head>
+<body>
+    @yield('body')
+    @yield('script')
+</body>
+</html>
