ef84097ccf3864ee2689a63a0e155b125653c811
Mmouky <33157839+Mmouky@users.noreply.github.com>
2019-02-05 13:21:37 +0100

    navbar

--- limit/resources/views/template.blade.php
+++ limit/resources/views/template.blade.php
@@ -7,10 +7,32 @@
     @yield('title')
 
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
-
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
 </head>
 <body>
-    @yield('body')
-    @yield('script')
+<nav class="navbar navbar-expand-sm bg-light navbar-light">
+    <ul class="navbar-nav">
+        <li class="nav-item active">
+            <a class="nav-link" href="/play">Jouer</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="/leaderboard">Classement</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="/ideas">Idées</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="/register">S'inscrire</a>
+        </li>
+    </ul>
+</nav>
+
+@yield('body')
+
+<script src="{{ asset('js/jquery-3.3.1.min.js') }}"></script>
+<script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+
+@yield('script')
+
 </body>
 </html>
